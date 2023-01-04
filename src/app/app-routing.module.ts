0488093cbbf664ea30ccbba5c151bcdecefcca14
HungryForKnowledge12 <blaz.pogelsek@gmail.com>
2023-01-04 23:18:42 +0100

    homepage, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BuyComponent } from './buy/buy.component';
 
-const routes: Routes = [];
+// Določimo poti za naše komponente
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'buy', component: BuyComponent }
+];
 
+// Ustvarimo modul za rutiranje
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
